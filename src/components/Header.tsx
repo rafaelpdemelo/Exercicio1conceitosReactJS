dc6bcf85961b28ba11da2ee784262d7a5433575d
Rafael Pereira <rafaelpdemelo@gmail.com>
2021-11-11 18:35:36 -0300

    altearções com id visual

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,7 @@ export function Header() {
   return (
     <header className="header">
       <div>
+      <img class="imgLogo" src="/simbolo 02.png"/>
         <img src="/logo.svg" alt="to.do"/>
       </div>
     </header>
